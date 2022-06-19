f1cdfaf84021215b66c9d008dddc3ff2c05abb8f
subhanprime <subhan25902@gmail.com>
2022-06-19 08:38:12 +0500

    myfirst react comment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Final from './forms/form.js'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>subhan ali</h1>
+     <Final/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/forms/form.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const myForms = () => {
+return<>
+<h1>subhana ali form</h1>
+</>
+}
+export default myForms
